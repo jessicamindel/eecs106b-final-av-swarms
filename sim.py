75bc3e252525a3ca044b48492a3fe64717dad755
Jessie Mindel <jessie.mindel@gmail.com>
2021-05-03 21:24:41 -0700

    Move sim utils into dedicated utils file

--- sim.py
+++ sim.py
@@ -1,79 +1,8 @@
 import numpy as np
-
+from utils import *
 
 # https://gist.github.com/danieljfarrell/faf7c4cafd683db13cbc
 # public domain
-
-def magnitude(vector):
-   return np.sqrt(np.dot(np.array(vector),np.array(vector)))
-
-def norm(vector):
-   return np.array(vector)/magnitude(np.array(vector))
-
-def intersect_ray_segment(rayOrigin, angle, point1, point2):
-    """
-    >>> # Line segment
-    >>> z1 = (0,0)
-    >>> z2 = (10, 10)
-    >>>
-    >>> # Test ray 1 -- intersecting ray
-    >>> r = (0, 5)
-    >>> d = norm((1,0))
-    >>> len(lineRayIntersectionPoint(r,d,z1,z2)) == 1
-    True
-    >>> # Test ray 2 -- intersecting ray
-    >>> r = (5, 0)
-    >>> d = norm((0,1))
-    >>> len(lineRayIntersectionPoint(r,d,z1,z2)) == 1
-    True
-    >>> # Test ray 3 -- intersecting perpendicular ray
-    >>> r0 = (0,10)
-    >>> r1 = (10,0)
-    >>> d = norm(np.array(r1)-np.array(r0))
-    >>> len(lineRayIntersectionPoint(r0,d,z1,z2)) == 1
-    True
-    >>> # Test ray 4 -- intersecting perpendicular ray
-    >>> r0 = (0, 10)
-    >>> r1 = (10, 0)
-    >>> d = norm(np.array(r0)-np.array(r1))
-    >>> len(lineRayIntersectionPoint(r1,d,z1,z2)) == 1
-    True
-    >>> # Test ray 5 -- non intersecting anti-parallel ray
-    >>> r = (-2, 0)
-    >>> d = norm(np.array(z1)-np.array(z2))
-    >>> len(lineRayIntersectionPoint(r,d,z1,z2)) == 0
-    True
-    >>> # Test ray 6 --intersecting perpendicular ray
-    >>> r = (-2, 0)
-    >>> d = norm(np.array(z1)-np.array(z2))
-    >>> len(lineRayIntersectionPoint(r,d,z1,z2)) == 0
-    True
-    """
-    # Convert to numpy arrays
-    rayOrigin = np.array(rayOrigin, dtype=np.float)
-    rayDirection = np.array([np.cos(angle), np.sin(angle)])
-    point1 = np.array(point1, dtype=np.float)
-    point2 = np.array(point2, dtype=np.float)
-    
-    # Ray-Line Segment Intersection Test in 2D
-    # http://bit.ly/1CoxdrG
-    v1 = rayOrigin - point1
-    v2 = point2 - point1
-    v3 = np.array([-rayDirection[1], rayDirection[0]])
-    t1 = np.cross(v2, v1) / np.dot(v2, v3)
-    t2 = np.dot(v1, v3) / np.dot(v2, v3)
-    if t1 >= 0.0 and t2 >= 0.0 and t2 <= 1.0:
-        return t1
-    return -1
-
-# https://stackoverflow.com/questions/3838329/how-can-i-check-if-two-segments-intersect
-def ccw(A,B,C):
-    return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
-
-# Return true if line segments AB and CD intersect
-def intersect_segments(A,B,C,D):
-    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
-
 
 PHI_MIN = -np.pi/2
 PHI_MAX = np.pi/2 
