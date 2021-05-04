bda387aac9a6b6d4e1216ee748ba4465e1836d86
Jessie Mindel <jessie.mindel@gmail.com>
2021-05-04 00:30:18 -0700

    Add info on next steps to main.py

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 	s = Sim(args.num_cars, args.map_path, args.path_reversal_prob or 0, args.angle_min or -np.pi, args.angle_max or np.pi)
 	s.map.render(s.cars, pause_length=3)
+
+	# FIXME: Some problems right off the bat:
+	# - Cars seem to spawn in the wrong location--it's the top half, not the bottom half.
+	# - Cars' spawn angles aren't terribly diverse.
+	# - It's hard to tell what direction a car is pointing in. Maybe add arrows to the render?
+	# - I can't keep the plt window open without pausing it for a long period of time.
