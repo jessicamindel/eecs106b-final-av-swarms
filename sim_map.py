232823d2d02c8aff77ce435879bac993732f1732
Jessie Mindel <jessie.mindel@gmail.com>
2021-05-05 02:29:55 -0700

    Add auto alignment

--- sim_map.py
+++ sim_map.py
@@ -28,17 +28,25 @@ class Map:
 		# FIXME: These points might be iffy because right now, the origin is the top left, and it... increases as it goes down and right? Odd.
 		# Right now, I'm flipping it so that the origin is the bottom left and it increases as it goes up and right; not sure if that fixes it.
 
-		start_points = np.where(np.logical_and(img_r < 250, img_g < 250, img_b == 255))
+		# Start direction points are used to calculate the starting angle; they form a line that bounds the start section pointing in the direction
+		# that cars in that section should go. In the future, we could add a similar feature for the end point, but we currently don't include theta
+		# in the goal state.
+		start_dir_points = np.where(np.logical_and(img_r == 0, np.logical_and(img_g == 0, img_b == 255)))
+		self.start_dir_points = np.ones((len(start_dir_points[0]), 2))
+		self.start_dir_points[:,0] = start_dir_points[1] # col = x
+		self.start_dir_points[:,1] = start_dir_points[0] # row = y
+
+		start_points = np.where(np.logical_and(np.logical_and(img_r < 250, img_r > 0), np.logical_and(np.logical_and(img_g < 250, img_g > 0), img_b == 255)))
 		self.start_points = np.zeros((len(start_points[0]), 2))
 		self.start_points[:,0] = start_points[1] # col = x
 		self.start_points[:,1] = start_points[0] # row = y
 
-		end_points = np.where(np.logical_and(img_r == 255, img_g < 250, img_b < 250))
+		end_points = np.where(np.logical_and(img_r == 255, np.logical_and(img_g < 250, img_b < 250)))
 		self.end_points = np.zeros((len(end_points[0]), 2))
 		self.end_points[:,0] = end_points[1] # col = x
 		self.end_points[:,1] = end_points[0] # row = y
 
-		boundary_points = np.where(np.logical_and(img_r == 0, img_g == 0, img_b == 0))
+		boundary_points = np.where(np.logical_and(img_r == 0, np.logical_and(img_g == 0, img_b == 0)))
 		self.boundary_points = np.zeros((len(boundary_points[0]), 2))
 		self.boundary_points[:,0] = boundary_points[1] # col = x
 		self.boundary_points[:,1] = boundary_points[0] # row = y
@@ -57,7 +65,7 @@ class Map:
 	def get_car_size(self):
 		return self.car_width, self.car_height
 
-	def choose_path(self, padding=0):
+	def choose_path(self, padding=0, angle_mode='auto'):
 		'''Finds a starting and ending point for a car. Returns each as (row, col) indices.'''
 		# Randomly select a start point
 		start_idx = rng.integers(self.start_points.shape[0])
@@ -70,8 +78,20 @@ class Map:
 			end_temp = end
 			end = start
 			start = end_temp
-		# Randomly choose an angle
-		start_angle = rng.uniform(low=self.angle_min, high=self.angle_max)
+		if angle_mode[:4] == 'auto':
+			# Find the nearest point along the direction line
+			nearest_dir_idx = np.argmin(np.linalg.norm(self.start_dir_points - start, axis=1))
+			dir_vec = start - self.start_dir_points[nearest_dir_idx]
+			# Determine the angle using the resulting vector
+			start_angle = np.mod(-np.arctan2(*dir_vec), 2*np.pi)
+			if angle_mode == 'auto_noise':
+				# TODO: Use Gaussian noise with parameter for max amount of noise centered at 0, stddev = that parameter
+				pass
+		elif angle_mode == 'random':
+			# Randomly choose an angle
+			start_angle = rng.uniform(low=self.angle_min, high=self.angle_max)
+		else:
+			raise ValueError(f'In Map.choose_path, angle_mode must be either \'auto\', \'auto_noise\', or \'random\'; {angle_mode} is not a valid mode.')
 		# Ensure no collisions
 		if self.car_has_boundary_collision(start, start_angle, padding):
 			return self.choose_path(padding)
@@ -91,6 +111,7 @@ class Map:
 		# FIXME: On second thought, will this miss things that are in the box of a
 		# boundary pixel but aren't exactly on that pixel's corner?
 
+	# FIXME: It's possible I have the y direction wrong. This will need a revisit.
 	def raycast(self, x, y, angle):
 		# Rotate image into these coordinates for purely horizontal ray traversal
 		R = rot_matrix(-angle)
